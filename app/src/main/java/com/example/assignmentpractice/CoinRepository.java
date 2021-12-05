747a0f242886856f29bea4ac956fde4a4bb0716a
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-12-05 17:09:33 +0000

    Added CoinRepo

---
+++ app/src/main/java/com/example/assignmentpractice/CoinRepository.java
@@ -0,0 +1,42 @@
+package com.example.assignmentpractice;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class CoinRepository {
+    private CoinDAO mCoinDao;
+    private LiveData<List<Coin>> mAllCoins;
+
+    CoinRepository(Application application) {
+        CoinRoomDatabase db = CoinRoomDatabase.getDatabase(application);
+        mCoinDao = db.coinDao();
+        mAllCoins = mCoinDao.getAllCoins();
+    }
+
+    LiveData<List<Coin>> getAllCoins() {
+        return mAllCoins;
+    }
+
+    public void insert (Coin coin) {
+        new insertAsyncTask(mCoinDao).execute(coin);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Coin, Void, Void> {
+
+        private CoinDAO mAsyncTaskDao;
+
+        insertAsyncTask(CoinDAO dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Coin... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
