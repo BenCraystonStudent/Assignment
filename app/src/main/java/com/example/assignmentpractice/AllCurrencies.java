8d6d556bfe0e5f73cbee9ac5f420d27bc4253b5f
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-12-13 14:13:20 +0000

    Fragments added, going to mess around with Main now

--- app/src/main/java/com/example/assignmentpractice/AllCurrencies.java
+++ app/src/main/java/com/example/assignmentpractice/AllCurrencies.java
@@ -6,13 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class AllCurrencies extends Fragment {
     private final LinkedList<String> mCoinList = new LinkedList<>();
+    private final LinkedList<Coin> coins = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private CoinListAdapter aAdapter;
     private View aView;
@@ -28,6 +32,13 @@ public class AllCurrencies extends Fragment {
         aView = inflater.inflate(R.layout.fragment_all_currencies, container, false);
 
         mRecyclerView = aView.findViewById(R.id.recyclerview);
+        LinearLayoutManager llm = new LinearLayoutManager(aContext);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+        aAdapter = new CoinListAdapter((AppCompatActivity) aContext, coins);
+        mRecyclerView.setAdapter(aAdapter);
+
+        return aView;
     }
 }
 
--- app/src/main/java/com/example/assignmentpractice/FavCurrencies.java
+++ app/src/main/java/com/example/assignmentpractice/FavCurrencies.java
@@ -6,13 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class FavCurrencies extends Fragment {
     private final LinkedList<String> mCoinList = new LinkedList<>();
+    private final LinkedList<Coin> coins = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private PortfolioListAdapter pAdapter;
     private View pView;
@@ -25,9 +29,16 @@ public class FavCurrencies extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        pView = inflater.inflate(R.layout.fragment_all_currencies, container, false);
+        pView = inflater.inflate(R.layout.fragment_fav_currencies, container, false);
 
         mRecyclerView = pView.findViewById(R.id.recyclerview);
+        LinearLayoutManager llm = new LinearLayoutManager(pContext);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
+        pAdapter = new PortfolioListAdapter((AppCompatActivity) pContext, coins);
+        mRecyclerView.setAdapter(pAdapter);
+
+        return pView;
     }
 }
 
