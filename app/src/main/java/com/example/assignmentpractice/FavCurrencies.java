f1387ba6806ff56e49af1a51c17ef44d6029ad93
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-12-14 18:35:56 +0000

    ViewPager, Adapters working!

--- app/src/main/java/com/example/assignmentpractice/FavCurrencies.java
+++ app/src/main/java/com/example/assignmentpractice/FavCurrencies.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -29,6 +32,8 @@ public class FavCurrencies extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        CoinViewModel mCoinViewModel = new ViewModelProvider(this).get(CoinViewModel.class);
+
         pView = inflater.inflate(R.layout.fragment_fav_currencies, container, false);
         pContext = container.getContext();
         pRecyclerView = pView.findViewById(R.id.recyclerview);
@@ -38,7 +43,22 @@ public class FavCurrencies extends Fragment {
         pAdapter = new PortfolioListAdapter( pContext, coins);
         pRecyclerView.setAdapter(pAdapter);
 
+        mCoinViewModel.getAllCoins().observe(getViewLifecycleOwner(), new Observer<List<Coin>>()
+        {
+            @Override
+            public void onChanged(@Nullable final List<Coin> coins) {
+                // Update the cached copy of the words in the adapter.
+                pAdapter.setCoins(coins);
+            }
+        });
+
         return pView;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        getActivity();
     }
 }
 
