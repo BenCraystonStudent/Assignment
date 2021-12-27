6e552eb74df8fb9d9abab9caf1f3e50d9537cd40
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-12-27 20:05:40 +0000

    Graphs not added, API call made successfully, need description in TextView

--- app/src/main/java/com/example/assignmentpractice/CoinInfo.java
+++ app/src/main/java/com/example/assignmentpractice/CoinInfo.java
@@ -12,34 +12,88 @@ import okhttp3.Response;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Iterator;
 import java.util.Objects;
 
 //TODO: Add selected currency to the URI
 
 public class CoinInfo extends AppCompatActivity {
+    private TextView currencyDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_info);
-
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+        try {
+            getHTTPData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        currencyDescription = findViewById(R.id.currencyDescription);
+        currencyDescription.setText("Fuck off");
     }
 
+
+    void getHTTPData() throws IOException {
+        /* https://www.coingecko.com/api/documentations/v3#/ */
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder =
+                Objects.requireNonNull(HttpUrl.parse("https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false"))
+                        .newBuilder();
+
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                JSONObject graphData;
+
+                final String myResponse = Objects.requireNonNull(response.body(), "Invalid Null API Response Received").string();
+
+                Log.d("OkHTTPResponse", "API Call Successful");
+                response.close();
+
+                try {
+                    JSONObject JSONData = new JSONObject(myResponse);
+                    String parsedDescription = JSONData.get("description").toString();
+
+
+                } catch (JSONException e) {
+                    Log.d("OkHTTPResponse", "JSON Format Problem");
+                    e.printStackTrace();
+                }
+                runOnUiThread(() -> {
+                    Log.d("OkHTTPResponse", myResponse);
+
+                });
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d("OkHTTPResponse","The API call for the coins failed: "
+                        + e.getMessage());
+                call.cancel();
+            }
+
+
+        });
+    }
 }
+
