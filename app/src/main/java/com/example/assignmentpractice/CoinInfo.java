fd775f5ea598e28f8504b9d136e2f16504d506af
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-12-28 17:01:08 +0000

    DESCRIPTIONS NOW ADDED

--- app/src/main/java/com/example/assignmentpractice/CoinInfo.java
+++ app/src/main/java/com/example/assignmentpractice/CoinInfo.java
@@ -36,7 +36,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 
-//TODO: Add selected currency to the URI
 
 public class CoinInfo extends AppCompatActivity {
     private TextView currencyDescription;
