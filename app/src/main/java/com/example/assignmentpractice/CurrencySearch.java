1e2629ee338e1f686fac768b0d14279e504408ac
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-01-03 22:46:06 +0000

    SEARCH FUNCTION RETURNING SPECIFIC COIN INCLUDING INFO (SOMEHOW)

--- app/src/main/java/com/example/assignmentpractice/CurrencySearch.java
+++ app/src/main/java/com/example/assignmentpractice/CurrencySearch.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
