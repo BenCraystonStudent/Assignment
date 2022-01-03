b5ec520a0e93921551e8a52ac0a0ca6d95a3d658
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-01-03 18:40:33 +0000

    Search stuff added but activity not being called yet

--- app/src/main/java/com/example/assignmentpractice/CurrencySearch.java
+++ app/src/main/java/com/example/assignmentpractice/CurrencySearch.java
@@ -1,14 +1,99 @@
 package com.example.assignmentpractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class CurrencySearch extends AppCompatActivity {
+    private RecyclerView searchRecycler;
+    private CoinListAdapter sAdapter;
+    private LinkedList<Coin> searchedCoins;
+    private View sView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_search);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        sAdapter  = new CoinListAdapter((AppCompatActivity) this, searchedCoins);
+        searchRecycler = findViewById(R.id.recyclerview);
+        searchRecycler.setLayoutManager(llm);
+        searchRecycler.setAdapter(sAdapter);
+
+
+            OkHttpClient client = new OkHttpClient();
+            HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse("https://api.coingecko.com/api/v3/coins/" + query).newBuilder());
+
+            String url = urlBuilder.build().toString();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    Log.d("Response Failed", "Nothing sent back from CoinGecko");
+                    call.cancel();
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    final String myResponse = Objects.requireNonNull(response.body(), "Response Received").string();
+                    Log.d("OkHTTPResponse", "API Call Successful");
+                    response.close();
+
+                    try {
+                        JSONObject oJSON = new JSONObject(myResponse);
+                        searchedCoins.clear();
+
+                        double CoinValue;
+                        /* Build the list of coins from API Data */
+                        for (Iterator<String> it = oJSON.keys(); it.hasNext(); ) {
+                            String coinName = it.next();
+                            CoinValue = oJSON.getJSONObject(coinName).getDouble("gbp");
+                            searchedCoins.add(new Coin(coinName, "gbp", CoinValue, 0.0));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.d("OkHTTPResponse", "JSON Format Problem");
+                    }
+
+                    runOnUiThread(() -> {
+                        Log.d("OkHTTPResponse", myResponse);
+                        /* Update spinner with new coin data */
+                        searchedCoins.sort(new SortByCoinName());
+                        sAdapter.setCoins(searchedCoins);
+                    });
+                }
+            });
+        }
     }
 }
