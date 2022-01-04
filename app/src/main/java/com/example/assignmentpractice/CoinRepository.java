f3213a6b43d8990865f24b15d8c557e74ddb6170
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-01-04 14:08:53 +0000

    ASyncTask configured (maybe)

--- app/src/main/java/com/example/assignmentpractice/CoinRepository.java
+++ app/src/main/java/com/example/assignmentpractice/CoinRepository.java
@@ -26,8 +26,9 @@ public class CoinRepository {
         new insertAsyncTask(mCoinDao).execute(coin);
     }
 
-    public void update(String coin_name, Double c_held) {
-        mCoinDao.update(c_held, coin_name);
+    public void update(Coin coin) {
+        mCoinDao.update(coin);
+        new updateAsyncTask(mCoinDao).execute(coin);
     }
 
     private static class insertAsyncTask extends AsyncTask<Coin, Void, Void> {
@@ -41,6 +42,37 @@ public class CoinRepository {
         @Override
         protected Void doInBackground(final Coin... params) {
             mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateParams{
+        Double c_held;
+        String coin_name;
+
+        UpdateParams(Double c_held, String coin_name){
+            this.c_held = c_held;
+            this.coin_name = coin_name;
+        }
+    }
+
+    public void UpdateCurrencyHeld(Double c_held, String coin_name){
+        UpdateParams params = new UpdateParams(c_held, coin_name);
+        new updateAsyncTask(mCoinDao).execute((Runnable) params);
+    }
+
+    private static class updateAsyncTask extends AsyncTask<Coin, Void, Void>
+    {
+        private CoinDAO uAsyncTaskDao;
+
+        updateAsyncTask(CoinDAO dao){uAsyncTaskDao = dao;}
+
+        @Override
+        protected Void doInBackground(final Coin... params)
+        {
+            Double amount = params[0].mCurrencyHeld;
+            String name = params[0].mCoin;
+            uAsyncTaskDao.update(amount, name);
             return null;
         }
     }
