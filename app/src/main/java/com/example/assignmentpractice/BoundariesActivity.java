678bca98682e7fc988246cb7cf5ff89326b4941e
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-01-12 17:28:48 +0000

    "Currently trading at" implemented

--- app/src/main/java/com/example/assignmentpractice/BoundariesActivity.java
+++ app/src/main/java/com/example/assignmentpractice/BoundariesActivity.java
@@ -1,6 +1,8 @@
 package com.example.assignmentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,11 +11,24 @@ import android.widget.TextView;
 public class BoundariesActivity extends AppCompatActivity {
     private Button increaseButton, decreaseButton;
     private TextView currentValueTextView;
+    private CoinViewModel cvm;
+    private String thresholdsCoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boundaries);
+        currentValueTextView = findViewById(R.id.currentValue);
 
+        Bundle extras = getIntent().getExtras();
+        thresholdsCoin = extras.getString("thresholdsCoin");
+
+        cvm = new ViewModelProvider(this).get(CoinViewModel.class);
+        cvm.currentValue(thresholdsCoin).observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                currentValueTextView.setText("Currently trading at £" + aDouble);
+            }
+        });
     }
 }
